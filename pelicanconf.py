00bdaebcd1b855bd6f82431650cca1141a789cb1
johnskiller <johnskiller@gmail.com>
2013-06-03 15:47:54 +0800

    remove unused links

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,11 +35,10 @@ TRANSLATION_FEED_ATOM = None
 LINKS =  (('Pelican', 'http://getpelican.com/'),
           ('Python.org', 'http://python.org/'),
           ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+          )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'http://twitter.com/johnskiller'),)
 
 DEFAULT_PAGINATION = 5
 DISPLAY_PAGES_ON_MENU = True
